7873405c1abd96c6254ade71b849aa69ab904dba
Mraleksa <a.gribanovskiy@gmail.com>
2017-03-16 20:20:06 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,6 +1,6 @@
 // This is a template for a Node.js scraper on morph.io (https://morph.io)
 
-var Scraper = require('../lib/index.js');
+var Scraper = require('index.js');
 console.log("hi")
 console.log(Scraper)
 
