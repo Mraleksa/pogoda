55321c2f4c57db7d1b78560b5a259b22df1d2ddd
Mraleksa <a.gribanovskiy@gmail.com>
2017-03-16 20:36:26 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,13 +1,13 @@
 var sqlite3 = require("sqlite3").verbose();
 
-# Open a database handle
+// Open a database handle
 var db = new sqlite3.Database("data.sqlite");
 db.serialize(function() {
 
-  # Create new table
+  // Create new table
   db.run("CREATE TABLE IF NOT EXISTS data (title TEXT)");
 
-  # Insert a new record
+ // Insert a new record
   var statement = db.prepare("INSERT INTO data(title) VALUES (?)");
   statement.run('A new title to add');
   statement.finalize();
