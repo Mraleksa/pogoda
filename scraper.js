c7a2327d3bef9cb02da647f6683a87ed778a1d42
Mraleksa <a.gribanovskiy@gmail.com>
2017-03-16 20:21:13 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,6 +1,14 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+var sqlite3 = require("sqlite3").verbose();
 
-var Scraper = require('index.js');
-console.log("hi")
-console.log(Scraper)
+# Open a database handle
+var db = new sqlite3.Database("data.sqlite");
+db.serialize(function() {
 
+  # Create new table
+  db.run("CREATE TABLE IF NOT EXISTS data (title TEXT)");
+
+  # Insert a new record
+  var statement = db.prepare("INSERT INTO data(title) VALUES (?)");
+  statement.run('A new title to add');
+  statement.finalize();
+});
